c17fff04d19295f8c9dd012af72156caef8df76f
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-05-04 20:46:32 +0900

    use `@astrojs/rss` instead of getStaticPaths

---
+++ src/pages/posts-feed.xml.js
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+
+const postImportResult = import.meta.globEager("./*.md");
+let posts = Object.values(postImportResult);
+posts = posts.sort(
+	(a, b) => Date.parse(b.frontmatter.published) - Date.parse(a.frontmatter.published)
+);
+
+export const get = () =>
+	rss({
+		title: "記事: Yuhei Yasuda",
+		description: "日記や技術情報など",
+		items: posts.map((post) => ({
+			link: post.url,
+			title: post.frontmatter.title,
+			pubDate: post.frontmatter.published,
+		})),
+		canonicalUrl: "https://yuheiy.com", // fixme
+	});
