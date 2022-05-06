f9affdc78d9552adc87d5ba6f9a799781c8c227c
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-05-06 14:42:56 +0900

    fix rss options

--- src/pages/posts-feed.xml.js
+++ src/pages/posts-feed.xml.js
@@ -10,10 +10,10 @@ export const get = () =>
 	rss({
 		title: "記事: Yuhei Yasuda",
 		description: "日記や技術情報など",
+		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
 			link: post.url,
 			title: post.frontmatter.title,
 			pubDate: post.frontmatter.published,
 		})),
-		canonicalUrl: "https://yuheiy.com", // fixme
 	});
