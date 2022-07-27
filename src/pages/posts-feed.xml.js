57527b2a15540fc0b6e495619c630928fe3f6bee
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-07-27 17:44:25 +0900

    update posts-feed

--- src/pages/posts-feed.xml.js
+++ src/pages/posts-feed.xml.js
@@ -1,13 +1,13 @@
 import rss from "@astrojs/rss";
 
-const postImportResult = import.meta.globEager("./*.md");
+const postImportResult = import.meta.glob("./*.md", { eager: true });
 const posts = Object.values(postImportResult);
 posts.sort((a, b) => Date.parse(b.frontmatter.published) - Date.parse(a.frontmatter.published));
 
 export const get = () =>
 	rss({
 		title: "Yuhei Yasudaの記事",
-		description: "日記や技術情報など",
+		description: "日記やウェブ開発について",
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
 			link: post.url,
