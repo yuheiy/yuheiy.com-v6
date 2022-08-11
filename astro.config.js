ff2ef86c9e947de18347733f85b894d39f880361
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-08-11 13:37:42 +0900

    set img attributes automatically

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,14 @@
+import path from "node:path";
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { Window } from "happy-dom";
+import sizeOf from "image-size";
 import { toString } from "mdast-util-to-string";
 import { select } from "unist-util-select";
+import { visit } from "unist-util-visit";
 
 export default defineConfig({
 	site: "https://yuheiy.com",
@@ -14,6 +18,7 @@ export default defineConfig({
 			theme: "css-variables",
 		},
 		remarkPlugins: [remarkInjectDescription],
+		rehypePlugins: [rehypeAutoImageAttributes],
 	},
 	integrations: [
 		image(),
@@ -31,5 +36,29 @@ function remarkInjectDescription() {
 	return (tree, { data }) => {
 		const firstParagraph = select("paragraph", tree);
 		data.astro.frontmatter.description = toString(firstParagraph);
+	};
+}
+
+function rehypeAutoImageAttributes() {
+	const { document } = new Window();
+
+	return (tree) => {
+		visit(tree, "raw", (node) => {
+			document.body.innerHTML = node.value;
+
+			for (const imgEl of Array.from(document.querySelectorAll("img"))) {
+				if (!imgEl.hasAttribute("width") || !imgEl.hasAttribute("height")) {
+					const { width, height } = sizeOf(path.join("public", imgEl.src));
+					imgEl.width = width;
+					imgEl.height = height;
+				}
+
+				if (!imgEl.hasAttribute("decoding")) {
+					imgEl.setAttribute("decoding", "async");
+				}
+			}
+
+			node.value = document.body.innerHTML;
+		});
 	};
 }
