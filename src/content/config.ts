1b4a0a671bb58fe84bbddfb8e71ad8f702f4015f
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2023-01-28 21:57:58 +0900

    inlining blogCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,12 @@
 import { z, defineCollection } from "astro:content";
 
-const blogCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		publishDate: z.string(),
-		ogImageURL: z.string().optional(),
-		twitterCard: z.string().optional(),
-	}),
-});
-
 export const collections = {
-	blog: blogCollection,
+	blog: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			publishDate: z.string(),
+			ogImageURL: z.string().optional(),
+			twitterCard: z.string().optional(),
+		}),
+	}),
 };
