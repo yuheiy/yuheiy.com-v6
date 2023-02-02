4417d7bcec5d99a978902f8775948893b0c07ee9
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2023-02-02 15:51:36 +0900

    prefer soft tab

--- src/pages/posts-feed.xml.js
+++ src/pages/posts-feed.xml.js
@@ -4,19 +4,19 @@ import dayjs from "../lib/dayjs";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
-	const allBlogEntries = (await getCollection("blog")).sort(
-		(a, b) => dayjs(b.data.publishDate).tz().valueOf() - dayjs(a.data.publishDate).tz().valueOf()
-	);
+  const allBlogEntries = (await getCollection("blog")).sort(
+    (a, b) => dayjs(b.data.publishDate).tz().valueOf() - dayjs(a.data.publishDate).tz().valueOf()
+  );
 
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: allBlogEntries.map((entry) => ({
-			title: entry.data.title,
-			pubDate: entry.data.publishDate,
-			description: entry.data.description,
-			link: new URL(`/${entry.slug}`, import.meta.env.SITE).toString(),
-		})),
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: allBlogEntries.map((entry) => ({
+      title: entry.data.title,
+      pubDate: entry.data.publishDate,
+      description: entry.data.description,
+      link: new URL(`/${entry.slug}`, import.meta.env.SITE).toString(),
+    })),
+  });
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,80 +3,80 @@ const { container, dynamicColors, kerning } = require("./tailwind-plugins.cjs");
 
 /** @type {import("tailwindcss/types").Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		fontSize: {
-			// https://w3c.github.io/jlreq/#id343
-			xs: "0.75rem",
-			sm: "0.875rem",
-			base: "1rem",
-			lg: "1.125rem",
-			xl: "1.25rem",
-			"2xl": "1.5rem",
-			"3xl": "1.75rem",
-			"4xl": "2rem",
-			"5xl": "2.25rem",
-			"6xl": "2.5rem",
-		},
-		lineHeight: {
-			tight: "1.5",
-			normal: "1.8",
-		},
-		extend: {
-			borderColor: {
-				DEFAULT: "rgb(var(--dynamic-color-borderColor-DEFAULT) / <alpha-value>)",
-			},
-			fontFamily: {
-				sans: ["sans-serif"],
-			},
-		},
-	},
-	corePlugins: {
-		container: false,
-	},
-	plugins: [
-		require("@tailwindcss/container-queries"),
-		container,
-		dynamicColors({
-			light: {
-				backgroundColor: {
-					DEFAULT: colors.white,
-					variant: colors.slate["100"],
-				},
-				borderColor: {
-					DEFAULT: colors.gray["200"],
-				},
-				ringColor: {
-					DEFAULT: colors.gray["200"],
-				},
-				textColor: {
-					DEFAULT: colors.gray["800"],
-					muted: colors.gray["500"],
-				},
-				textDecorationColor: {
-					DEFAULT: colors.gray["400"],
-				},
-			},
-			dark: {
-				backgroundColor: {
-					DEFAULT: colors.zinc["900"],
-					variant: colors.zinc["800"],
-				},
-				borderColor: {
-					DEFAULT: colors.zinc["700"],
-				},
-				ringColor: {
-					DEFAULT: colors.zinc["700"],
-				},
-				textColor: {
-					DEFAULT: colors.zinc["50"],
-					muted: colors.zinc["400"],
-				},
-				textDecorationColor: {
-					DEFAULT: colors.zinc["400"],
-				},
-			},
-		}),
-		kerning,
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontSize: {
+      // https://w3c.github.io/jlreq/#id343
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.75rem",
+      "4xl": "2rem",
+      "5xl": "2.25rem",
+      "6xl": "2.5rem",
+    },
+    lineHeight: {
+      tight: "1.5",
+      normal: "1.8",
+    },
+    extend: {
+      borderColor: {
+        DEFAULT: "rgb(var(--dynamic-color-borderColor-DEFAULT) / <alpha-value>)",
+      },
+      fontFamily: {
+        sans: ["sans-serif"],
+      },
+    },
+  },
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require("@tailwindcss/container-queries"),
+    container,
+    dynamicColors({
+      light: {
+        backgroundColor: {
+          DEFAULT: colors.white,
+          variant: colors.slate["100"],
+        },
+        borderColor: {
+          DEFAULT: colors.gray["200"],
+        },
+        ringColor: {
+          DEFAULT: colors.gray["200"],
+        },
+        textColor: {
+          DEFAULT: colors.gray["800"],
+          muted: colors.gray["500"],
+        },
+        textDecorationColor: {
+          DEFAULT: colors.gray["400"],
+        },
+      },
+      dark: {
+        backgroundColor: {
+          DEFAULT: colors.zinc["900"],
+          variant: colors.zinc["800"],
+        },
+        borderColor: {
+          DEFAULT: colors.zinc["700"],
+        },
+        ringColor: {
+          DEFAULT: colors.zinc["700"],
+        },
+        textColor: {
+          DEFAULT: colors.zinc["50"],
+          muted: colors.zinc["400"],
+        },
+        textDecorationColor: {
+          DEFAULT: colors.zinc["400"],
+        },
+      },
+    }),
+    kerning,
+  ],
 };
