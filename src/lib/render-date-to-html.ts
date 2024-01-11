584de9472e9d0e2b01cf615d871b9a4ad62e498a
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-01-12 03:37:12 +0900

    separate render-date

---
+++ src/lib/render-date-to-html.ts
@@ -0,0 +1,24 @@
+import { SITE_LANG, SITE_REGION } from '../consts';
+
+const locale = `${SITE_LANG}-${SITE_REGION}`;
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'long',
+});
+
+export function renderDateToHtml(date: Date) {
+  let html = '';
+  html += `<time datetime="${date.toISOString()}">`;
+  html += dateFormatter.format(date);
+  html += `</time>`;
+  return html;
+}
+
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest;
+  test('renderDateToHtml', () => {
+    expect(renderDateToHtml(new Date('2000-01-01'))).toMatchInlineSnapshot(
+      `"<time datetime="2000-01-01T00:00:00.000Z">2000年1月1日</time>"`,
+    );
+  });
+}
