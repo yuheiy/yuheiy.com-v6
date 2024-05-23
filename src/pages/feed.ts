34f69673085f80acefb47f4cfed9b37c11c53405
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-05-24 02:45:49 +0900

    refactor promise tasks

--- src/pages/feed.ts
+++ src/pages/feed.ts
@@ -8,21 +8,20 @@ import { getBlogDescription } from '../lib/get-blog-description';
 export async function GET(context: APIContext) {
   invariant(context.site);
 
-  const items = await getCollection('blog')
-    .then((entries) =>
-      entries
-        .toSorted((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-        .map(async (entry) => {
-          const description = await getBlogDescription(entry);
-          return {
-            link: `/${entry.slug}`,
-            title: entry.data.title,
-            pubDate: entry.data.pubDate,
-            description,
-          };
-        }),
-    )
-    .then((promises) => Promise.all(promises));
+  const entries = await getCollection('blog');
+  const items = await Promise.all(
+    entries
+      .toSorted((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+      .map(async (entry) => {
+        const description = await getBlogDescription(entry);
+        return {
+          link: `/${entry.slug}`,
+          title: entry.data.title,
+          pubDate: entry.data.pubDate,
+          description,
+        };
+      }),
+  );
 
   return rss({
     title: siteTitle,
