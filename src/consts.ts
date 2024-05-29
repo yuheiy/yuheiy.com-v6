6e132d309ed2f31576dce4056cb51c47e4306dbe
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-05-30 00:10:36 +0900

    organize constants

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,6 @@ export const siteLang = 'ja';
 export const siteRegion = 'JP';
 export const siteEmail = 'yuhei.yasuda1003@gmail.com';
 export const siteTwitterId = '_yuheiy';
+export const siteGithubId = 'yuheiy';
 export const siteDescription =
   'フロントエンドウェブ開発者。HTMLやWAI-ARIA、CSS設計、レスポンシブデザイン、Jamstack、シングルページアプリケーション、アクセシビリティ、インターフェースデザインが関心領域。';
