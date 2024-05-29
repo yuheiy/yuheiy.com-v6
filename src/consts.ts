1b7f0a08a18f6856ce8905e344c540b77cea6621
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-05-30 00:07:58 +0900

    update markup for author

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const siteTitle = 'Yuhei Yasuda';
 export const siteLang = 'ja';
 export const siteRegion = 'JP';
+export const siteEmail = 'yuhei.yasuda1003@gmail.com';
 export const siteTwitterId = '_yuheiy';
 export const siteDescription =
   'フロントエンドウェブ開発者。HTMLやWAI-ARIA、CSS設計、レスポンシブデザイン、Jamstack、シングルページアプリケーション、アクセシビリティ、インターフェースデザインが関心領域。';
