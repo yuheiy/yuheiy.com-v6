de4f9cb5bf004df6be574ceb5ce50ec9bc00d882
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-01-01 21:12:36 +0900

    add the link to bluesky

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@ export const siteLang = 'ja';
 export const siteRegion = 'JP';
 export const siteEmail = 'yuhei.yasuda1003@gmail.com';
 export const siteTwitterId = '_yuheiy';
+export const siteBlueskyId = 'yuheiy.com';
 export const siteGithubId = 'yuheiy';
+export const siteHatenaId = 'yuheiy';
 export const siteDescription =
   'フロントエンドウェブ開発者。HTML/WAI-ARIA、CSS、JavaScript/TypeScript、ウェブサイトおよびアプリケーション開発、アクセシビリティ、インターフェースデザイン、デザインシステム、DevOpsなどが得意分野。';
