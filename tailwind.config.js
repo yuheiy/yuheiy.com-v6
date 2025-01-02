204e67dd00e5e477bc239a1e20c352337c116f4e
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-01-02 21:42:56 +0900

    reconsider font-sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,17 +46,12 @@ export default {
   safelist: ['astro-code'],
   theme: {
     fontSize: {
-      // https://w3c.github.io/jlreq/#id343
       xs: '0.75rem',
       sm: '0.875rem',
       base: '1rem',
       lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.75rem',
-      '4xl': '2rem',
-      '5xl': '2.25rem',
-      '6xl': '2.5rem',
+      xl: '1.375rem',
+      '2xl': '1.625rem',
     },
     lineHeight: {
       none: '1',
